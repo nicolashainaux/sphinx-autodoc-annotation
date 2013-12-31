04103a77e5206567bf4c17fd3aa2b1f0338f5ae0
Virgil Dupras <hsoft@hardcoded.net>
2013-12-31 12:56:47 -0500

    Fix a couple of sphinx warnings
    annotation-less signatures weren't correctly handled.

--- sphinx_autodoc_annotation.py
+++ sphinx_autodoc_annotation.py
@@ -3,6 +3,8 @@ import inspect
 from sphinx.ext.autodoc import FunctionDocumenter, MethodDocumenter
 
 def get_class_link(obj):
+    if obj == inspect.Signature.empty:
+        return None
     if inspect.isclass(obj):
         if inspect.isbuiltin(obj):
             return obj.__qualname__
@@ -12,20 +14,21 @@ def get_class_link(obj):
     return None
 
 def add_annotation_content(documenter):
+    obj = documenter.object
     try:
-        sig = inspect.signature(documenter.object)
+        sig = inspect.signature(obj)
     except ValueError:
         # Can't extract signature, do nothing
         return
     for param in sig.parameters.values():
         arg_link = get_class_link(param.annotation)
         if arg_link:
-            line = ":type %s: %s" % (param.name, arg_link)
+            line = ":type %s: %s\n" % (param.name, arg_link)
             documenter.add_line(line, '', 0)
     return_link = get_class_link(sig.return_annotation)
     if return_link:
-        line = ":rtype: %s" % return_link
-        documenter.add_line(line, '', 0)
+        line = ":rtype: %s\n" % return_link
+        documenter.add_line(line, inspect.getsourcefile(obj), inspect.getsourcelines(obj)[1])
 
 class MyFunctionDocumenter(FunctionDocumenter):
     def generate(self, *args, **kwargs):
